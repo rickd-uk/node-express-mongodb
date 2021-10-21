574a604d7455df1915b620891b188cabd562bb5f
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2021-10-21 17:41:15 +0900

    trying webhooks

--- server.js
+++ server.js
@@ -1,50 +1,53 @@
-const express = require("express");
+const express = require('express')
 // const bodyParser = require("body-parser"); /* deprecated */
-const cors = require("cors");
+const cors = require('cors')
 
-const app = express();
+const app = express()
 
-const process = require("process");
+const process = require('process')
 
 var corsOptions = {
-  origin: "http://localhost:8081"
-};
+  origin: 'http://localhost:8081',
+}
 
-app.use(cors(corsOptions));
+app.use(cors(corsOptions))
 
 // parse requests of content-type - application/json
-app.use(express.json());  /* bodyParser.json() is deprecated */
+app.use(express.json()) /* bodyParser.json() is deprecated */
 
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));   /* bodyParser.urlencoded() is deprecated */
+app.use(
+  express.urlencoded({ extended: true }),
+) /* bodyParser.urlencoded() is deprecated */
 
-const db = require("./app/models");
+const db = require('./app/models')
 db.mongoose
   .connect(db.url, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => {
-    console.log("Connected to the database!");
+    console.log('Connected to the database!')
   })
-  .catch(err => {
-    console.log("Cannot connect to the database!", err);
-    process.exit();
-  });
+  .catch((err) => {
+    console.log('Cannot connect to the database!', err)
+    process.exit()
+  })
 
 // simple route
-app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application.",
-   node_env: process.env.NODE_ENV,
-   port: process.env.PORT,
-   pid: process.pid
-  });
-});
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Checking whether webhooks will work',
+    node_env: process.env.NODE_ENV,
+    port: process.env.PORT,
+    pid: process.pid,
+  })
+})
 
-require("./app/routes/turorial.routes")(app);
+require('./app/routes/turorial.routes')(app)
 
 // set port, listen for requests
-const PORT = process.env.PORT || 4000 
+const PORT = process.env.PORT || 4000
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+  console.log(`Server is running on port ${PORT}.`)
+})
