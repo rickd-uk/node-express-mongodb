4c7983df250312f1d426937d9e91402bdac9ea6a
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2021-10-22 11:07:14 +0900

    SELinux is a pain

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ db.mongoose
 // simple route
 app.get('/', (req, res) => {
   res.json({
-    message: 'Hello Hello??',
+    message: 'SELinux is a pain, but...',
     node_env: process.env.NODE_ENV,
     port: process.env.PORT,
     pid: process.pid,
