87130118ea9ab2e313e204b3b0f00c8b6dad7b38
Rick Dumpleton <78909973+rickd-uk@users.noreply.github.com>
2021-10-22 10:52:58 +0900

    Anyone there?

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ db.mongoose
 // simple route
 app.get('/', (req, res) => {
   res.json({
-    message: 'TEST!!!!',
+    message: 'ANyone there??',
     node_env: process.env.NODE_ENV,
     port: process.env.PORT,
     pid: process.pid,
